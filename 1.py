9285697ae7d70d8584dc53078f43054533ea7fe9
Arch <you@example.com>
2020-09-30 01:18:33 +0300

    cycle for, csv, clean information

--- 1.py
+++ 1.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 def get_html(url):
     r = requests.get(url)
     return r.text
+
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -16,9 +16,6 @@ def get_data(html):
 def main():
     url = 'https://wordpress.org/'
     print(get_data(get_html(url)))
-
-
-
 
 
 if __name__ == "__main__":
